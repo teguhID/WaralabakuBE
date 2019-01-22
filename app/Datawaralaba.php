eca686f1b00e49ad1860a470d969cb798e8a0acb
teguhID <teguh.nugraha7@gmail.com>
2019-01-22 17:34:38 +0700

    make model datawaralaba

---
+++ app/Datawaralaba.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Datawaralaba extends Model
+{
+    protected $table = 'datawaralaba'; //table database
+    protected $fillable = ['nama','alamat','jenis','phone','email','web','modal','gerai','bep','fee','keuntungan']; //kolom yang akan di isi
+    
+}
