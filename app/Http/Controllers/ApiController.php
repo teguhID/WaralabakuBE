8f81cc6b75cf852205b08b791fffead864ebc7fc
teguhID <teguh.nugraha7@gmail.com>
2019-04-11 12:52:41 +0700

    finished api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,17 +8,188 @@ use App\DataWaralabaModel;
 
 class ApiController extends Controller
 {
-    public function index(Request $request)
-    {
-        $keuntungan = $request->input('keuntungan');
-        $fee = $request->input('fee');
-        return NilaiUtilityModel::where('keuntungan', '<=', $keuntungan)->where('fee', '=', $fee)->get();
+    public function getData1(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData2(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData3(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData4(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData5(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData6(){// OK 
+        return $this->query('jenis', '=', 'makanan', 'modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData7(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData8(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
     }
 
-    public function getData1(){
-        return json_encode(array('data' => NilaiUtilityModel::where('fee', '=', '1')->get()));
+    public function getData9(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
     }
-    public function getData2(){
-        return json_encode(array('data' => NilaiUtilityModel::where('fee', '=', '2')->get()));
+    public function getData10(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData11(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData12(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData13(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData14(){// OK 
+        return $this->query('jenis', '=', 'makanan', 'modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData15(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData16(){// OK
+        return $this->query('jenis', '=', 'makanan', 'modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+
+    public function getData17(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData18(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData19(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData20(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData21(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData22(){// OK 
+        return $this->query('jenis', '=', 'minuman', 'modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData23(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData24(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+
+    public function getData25(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData26(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData27(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData28(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData29(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData30(){// OK 
+        return $this->query('jenis', '=', 'minuman', 'modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData31(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData32(){// OK
+        return $this->query('jenis', '=', 'minuman', 'modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+
+    public function getData33(){// OK
+        return $this->query1('modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData34(){// OK
+        return $this->query1('modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData35(){// OK
+        return $this->query1('modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData36(){// OK
+        return $this->query1('modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData37(){// OK
+        return $this->query1('modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData38(){// OK 
+        return $this->query1('modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData39(){// OK
+        return $this->query1('modal', '<=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData40(){// OK
+        return $this->query1('modal', '<=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+
+    public function getData41(){// OK
+        return $this->query1('modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData42(){// OK
+        return $this->query1('modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData43(){// OK
+        return $this->query1('modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData44(){// OK
+        return $this->query1('modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData45(){// OK
+        return $this->query1('modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '<=', 3, 'keuntungan', '<=', 100);
+    }
+    public function getData46(){// OK 
+        return $this->query1('modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'tidak ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData47(){// OK
+        return $this->query1('modal', '>=', 7000000, 'gerai', '>=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+    public function getData48(){// OK
+        return $this->query1('modal', '>=', 7000000, 'gerai', '<=', 65, 'fee', '=', 'ada', 'bep', '>=', 3, 'keuntungan', '>=', 100);
+    }
+
+    public function query(
+        $jenis, $jenisStatement, $jenisValue,
+        $modal, $modalStatement, $modalValue,
+        $gerai, $geraiStatement, $geraiValue,
+        $fee, $feeStatement, $feeValue,
+        $bep, $bepStatement, $bepValue,
+        $keuntungan, $keuntunganStatement, $keuntunganValue
+    )
+    {
+        return json_encode(array('data' => DataWaralabaModel::where($jenis, $jenisStatement, $jenisValue)
+                                                                ->where($modal, $modalStatement, $modalValue)
+                                                                ->where($gerai, $geraiStatement, $geraiValue)
+                                                                ->where($fee, $feeStatement, $feeValue)
+                                                                ->where($bep, $bepStatement, $bepValue)
+                                                                ->where($keuntungan, $keuntunganStatement, $keuntunganValue)
+                                                                ->orderBy('hasil', 'desc')
+                                                                ->get()));
+    }
+    public function query1(
+        $modal, $modalStatement, $modalValue,
+        $gerai, $geraiStatement, $geraiValue,
+        $fee, $feeStatement, $feeValue,
+        $bep, $bepStatement, $bepValue,
+        $keuntungan, $keuntunganStatement, $keuntunganValue
+    )
+    {
+        return json_encode(array('data' => DataWaralabaModel::where($modal, $modalStatement, $modalValue)
+                                                                ->where($gerai, $geraiStatement, $geraiValue)
+                                                                ->where($fee, $feeStatement, $feeValue)
+                                                                ->where($bep, $bepStatement, $bepValue)
+                                                                ->where($keuntungan, $keuntunganStatement, $keuntunganValue)
+                                                                ->orderBy('hasil', 'desc')
+                                                                ->get()));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,24 +23,6 @@ Route::resource('nilaiutility', 'NilaiUtilityController');
 Route::get('test', 'DataWaralabaController@dataUtility');
 Route::get('result', 'NilaiUtilityController@finalResultView');
 
-Route::post('api', 'ApiController@index');
-Route::get('api/getData1', 'ApiController@getData1');
-Route::get('api/getData2', 'ApiController@getData2');
-Route::get('api/getData3', 'ApiController@getData3');
-Route::get('api/getData4', 'ApiController@getData4');
-Route::get('api/getData5', 'ApiController@getData5');
-Route::get('api/getData6', 'ApiController@getData6');
-Route::get('api/getData7', 'ApiController@getData7');
-Route::get('api/getData8', 'ApiController@getData8');
-Route::get('api/getData9', 'ApiController@getData9');
-Route::get('api/getData10', 'ApiController@getData10');
-Route::get('api/getData11', 'ApiController@getData11');
-Route::get('api/getData12', 'ApiController@getData12');
-Route::get('api/getData13', 'ApiController@getData13');
-Route::get('api/getData14', 'ApiController@getData14');
-Route::get('api/getData15', 'ApiController@getData15');
-Route::get('api/getData16', 'ApiController@getData16');
-Route::get('api/getData17', 'ApiController@getData17');
-Route::get('api/getData18', 'ApiController@getData18');
-Route::get('api/getData19', 'ApiController@getData19');
-Route::get('api/getData20', 'ApiController@getData20');
+for ($i=0; $i <= 48; $i++) { 
+    Route::get('api/getData' . $i, 'ApiController@getData' . $i);    
+}
